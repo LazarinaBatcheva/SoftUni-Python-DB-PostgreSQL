95b5f8f504c9341dbd04af24805239cd4370829d
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-06-06 13:19:09 +0200

    added task solutions

---
+++ 07_Database_Programmability_and_Transactions/exercise/08_deposit_money.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE PROCEDURE sp_deposit_money(
+    account_id INT,
+    money_amount NUMERIC(4)
+)
+AS
+$$
+    BEGIN
+        UPDATE accounts
+        SET
+            balance = balance + money_amount
+        WHERE
+            id = account_id;
+    END;
+$$
+LANGUAGE plpgsql;
+
+
+-- test code
+--
+-- CALL sp_deposit_money(1, 200);
+-- CALL sp_deposit_money(10, 500);
+-- CALL sp_deposit_money(14, 1000);
+--
+-- SELECT * FROM accounts WHERE id = 1;
+-- SELECT * FROM accounts WHERE id = 10;
+-- SELECT * FROM accounts WHERE id = 14;
